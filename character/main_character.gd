44fcffb3a6ea27e4e31d1d26787e375ee11832c4
RedBear332 <RedBear3322@gmail.com>
2025-05-04 16:03:18 +0300

    Обновление ассетов и героя
    Обновление ассетов и героя

---
+++ character/main_character.gd
@@ -0,0 +1,44 @@
+extends CharacterBody2D
+
+@onready var hero: AnimatedSprite2D = $AnimatedSprite2D
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+var can_move: bool = false
+
+func _ready() -> void:
+	$AudioStreamPlayer.play()
+	hero.play("Appearing")
+	await hero.animation_finished
+	can_move = true
+
+func _physics_process(delta: float) -> void:
+	if can_move == false: return
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	update_animation()
+	move_and_slide()
+
+func update_animation():
+	if velocity.x:
+		hero.play("Run")
+	else: hero.play("Idle")
+	if velocity.y < 0:
+		hero.play("Jump")
+	elif velocity.y > 0:
+		hero.play("Fall")
+	if velocity.x < 0:
+		hero.flip_h = 1
+	else: hero.flip_h = 0 
